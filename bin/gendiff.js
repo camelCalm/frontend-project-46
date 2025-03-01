34d6cd93a3e86f0e25803fedc85ce074fcee2d99
camelCalm <sabirovich2014@mail.ru>
2025-03-01 13:32:51 +0300

    #2, delete jest and codeclimate packet

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,8 @@ program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
     .helpOption('-h, --help', 'output usage information')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
